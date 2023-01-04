df6e161e268456c7ec58171f5e3b467de81b993b
tharunrede <therupallytharun@gmail.com>
2023-01-03 20:38:18 -0700

    Update Search_Insert_Position.go

--- leetcode/Search_Insert_Position.go
+++ leetcode/Search_Insert_Position.go
@@ -6,43 +6,42 @@ import (
 
 func main() {
 
-	initial_arr := []int{1, 2, 3, 4, 7, 8, 9}
+	initial_arr := []int{1, 3, 5, 6}
 	var target int
 	fmt.Println("Array is:", initial_arr)
 
 	fmt.Println("Enter any number to insert/ to see if it is in the array: ")
 	fmt.Scanln(&target)
+	fmt.Println(searchInsert(initial_arr, target))
 
-	flag := false
+}
+
+func searchInsert(nums []int, target int) int {
+
+	//flag := false
 	position_inserted := -1
 
-	for pos, val := range initial_arr {
+	for pos, val := range nums {
 		//fmt.Println("pos, val, target", pos, val, target)
 
 		if target == val {
 			//fmt.Println("in tar== val")
-			flag = true
+			//flag = true
 			position_inserted = pos
 			break
 		} else if target < val {
 			//fmt.Println("in tar> val")
-			initial_arr = append(initial_arr[:pos+1], initial_arr[pos:]...)
-			initial_arr[pos] = target
+			nums = append(nums[:pos+1], nums[pos:]...)
+			nums[pos] = target
 			position_inserted = pos
 			break
 
-		} else if target > initial_arr[len(initial_arr)-1] {
-			initial_arr = append(initial_arr, target)
-			position_inserted = len(initial_arr) - 1
+		} else if target > nums[len(nums)-1] {
+			nums = append(nums, target)
+			position_inserted = len(nums) - 1
 			break
 		}
 	}
 
-	if flag == false {
-		fmt.Println("Element not found but inserted at ", position_inserted, "index")
-	} else {
-		fmt.Println("Element found at", position_inserted, "index")
-	}
-
-	fmt.Println("Final initial_arr", initial_arr)
+	return position_inserted
 }
